6bb6cf87426c82cc1d1d02915f8bb0241ea52a9d
wachirarobbinson <ngirigacharobbinson@gmail.com>
2023-09-06 23:45:09 +0300

    done

--- lib/debug.py
+++ lib/debug.py
@@ -1,10 +1,16 @@
-#!/usr/bin/env python3
+# lib/sqlalchemy_sandbox.py
 
-from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-from sqlalchemy_sandbox import Student
+Base = declarative_base()
 
-engine = create_engine('sqlite:///students.db')
+class Student(Base):
+    __tablename__ = 'students'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
 
 if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+    engine = create_engine('sqlite:///students.db')
+    Base.metadata.create_all(engine)
--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -9,4 +9,21 @@ class Student(Base):
     pass
 
 if __name__ == '__main__':
-    pass
+    pass#!/usr/bin/env python3
+import os
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm  import declarative_base
+
+Base = declarative_base()
+
+
+class Student(Base):
+    __tablename__ = 'students'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///students.db')
+    Base.metadata.create_all(engine)
